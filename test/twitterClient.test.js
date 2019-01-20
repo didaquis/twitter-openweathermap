2e7863df9c42a91e0eab409249a5d2cc8f9e5cea
Dídac García <didaquis@gmail.com>
2019-01-20 21:17:32 +0100

    Refactored test

--- test/twitterClient.test.js
+++ test/twitterClient.test.js
@@ -5,50 +5,44 @@ const twitterClient = require('../src/lib/twitter');
 
 describe('Testing Twitter auth', () => {
 
-	let twClient;
-
 	let twitter_consumer_key;
 	let twitter_consumer_secret;
 	let twitter_access_token_key;
 	let twitter_access_token_secret;
 
-	before((done) => {
-		twClient = twitterClient;
-
+	before( () => {
 		twitter_consumer_key = process.env.TWITTER_CONSUMER_KEY;
 		twitter_consumer_secret = process.env.TWITTER_CONSUMER_SECRET;
 		twitter_access_token_key = process.env.TWITTER_ACCESS_TOKEN_KEY;
 		twitter_access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
-
-		done();
 	});
 
 	it('credentials should be defined', () => {
-		expect(twClient.options.consumer_key).to.equal(twitter_consumer_key);
-		expect(twClient.options.consumer_key).to.be.a('string');
+		expect(twitterClient.options.consumer_key).to.equal(twitter_consumer_key);
+		expect(twitterClient.options.consumer_key).to.be.a('string');
 
-		expect(twClient.options.consumer_secret).to.equal(twitter_consumer_secret);
-		expect(twClient.options.consumer_secret).to.be.a('string');
+		expect(twitterClient.options.consumer_secret).to.equal(twitter_consumer_secret);
+		expect(twitterClient.options.consumer_secret).to.be.a('string');
 
-		expect(twClient.options.access_token_key).to.equal(twitter_access_token_key);
-		expect(twClient.options.access_token_key).to.be.a('string');
+		expect(twitterClient.options.access_token_key).to.equal(twitter_access_token_key);
+		expect(twitterClient.options.access_token_key).to.be.a('string');
 
-		expect(twClient.options.access_token_secret).to.equal(twitter_access_token_secret);
-		expect(twClient.options.access_token_secret).to.be.a('string');
+		expect(twitterClient.options.access_token_secret).to.equal(twitter_access_token_secret);
+		expect(twitterClient.options.access_token_secret).to.be.a('string');
 	});
 
 	it('should be a valid object', () => {
-		expect(twClient).to.be.an('object');
-		assert.isObject(twClient);
+		expect(twitterClient).to.be.an('object');
+		assert.isObject(twitterClient);
 	});
 
 	it('Twitter api should have valid methods', () => {
-		expect(twClient).to.have.a.property('post');
-		expect(twClient).to.have.a.property('get');
+		expect(twitterClient).to.have.a.property('post');
+		expect(twitterClient).to.have.a.property('get');
 	});
 
 	it('should do a valid request', (done) => {
-		twClient.get('search/tweets', {q: 'Twitter'}, function(error, tweets, response) {
+		twitterClient.get('search/tweets', {q: 'Twitter'}, function(error, tweets, response) {
 			assert.isDefined(response.statusCode);
 			assert.isNumber(response.statusCode);
 
