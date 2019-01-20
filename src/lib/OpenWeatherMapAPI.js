cf684aa221290af14349acda18123d9253ab2210
Dídac García <didaquis@gmail.com>
2019-01-21 00:15:37 +0100

    Added first implementation of OpenWeatherMap API

---
+++ src/lib/OpenWeatherMapAPI.js
@@ -0,0 +1,52 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+/** Class representing a request to a public OpenWeatherMap API. */
+class OpenWeatherMapAPI {
+
+	/**
+	 * Create new request
+	 * @param {string} baseUrl Main URL of API endpoint
+	 * @param {string} lang Language of requested data
+	 * @param {string} units Units of requestes data
+	 */
+	constructor({ baseUrl, lang, units }) {
+		this.baseUrl = baseUrl;
+		this.lang = `lang=${lang}`;
+		this.units = `units=${units}`;
+		this.appId = `APPID=${process.env.OPENWEATHERMAP_APIKEY}`;
+	}
+
+	/**
+	 * Make a request to public API
+	 * @param {string} URL of endpoint
+	 * @returns {Promise<Response>} Data received from endpoint
+	 * @throws {string} If something goes wrong
+	 */
+	call(url) {
+		return fetch(url)
+			.then(res => {
+				const statusCode_OK = 200;
+				if (res.status !== statusCode_OK) {
+					throw new Error(res.status);
+				}
+
+				return res.json();
+			});
+	}
+
+	/**
+	 * Retrieve weather data
+	 * @param {number} idOfCity Identification code of city
+	 * @returns {Promise<Response>} Data received from endpoint
+	 */
+	getWeatherByIdOfCity(idOfCity) {
+		if (!idOfCity) {
+			throw new Error('Error: Invalid Id of city');
+		}
+		const urlOfRequest = `${this.baseUrl}?id=${idOfCity}&${this.appId}&${this.units}&${this.lang}`;
+		return this.call(urlOfRequest);
+	}
+}
+
+module.exports = OpenWeatherMapAPI;
---
+++ test/OpenWeatherMapAPI.test.js
@@ -0,0 +1,81 @@
+require('dotenv').config();
+const { assert, expect } = require('chai');
+
+const OpenWeatherMapAPI = require('../src/lib/OpenWeatherMapAPI');
+
+const appConfig = require('../src/appConfiguration');
+
+describe('Testing Open Weather Map API', () => {
+
+	let OWM_API;
+	let OWM_APIKEY;
+
+	before('Init API', () => {
+		OWM_API = new OpenWeatherMapAPI(appConfig.openWeatherMapAPI);
+		OWM_APIKEY = process.env.OPENWEATHERMAP_APIKEY;
+	});
+
+	describe('API should be load correctly', () => {
+		it('should load object', () => {
+			expect(OWM_API).to.be.an('object');
+		});
+
+		it('should load functions', () => {
+			expect(OWM_API.getWeatherByIdOfCity).to.be.a('function');
+			expect(OWM_API).to.have.a.property('getWeatherByIdOfCity');
+			expect(OWM_API).to.have.a.property('call');
+		});
+
+		it('should have properties defined', () => {
+			expect(OWM_API).to.have.a.property('baseUrl');
+			expect(OWM_API).to.have.a.property('lang');
+			expect(OWM_API).to.have.a.property('units');
+			expect(OWM_API).to.have.a.property('appId');
+
+			expect(OWM_API.baseUrl).to.be.a('string');
+			expect(OWM_API.lang).to.be.a('string');
+			expect(OWM_API.units).to.be.a('string');
+			expect(OWM_API.appId).to.be.a('string');
+
+			expect(OWM_API.baseUrl).to.equal(appConfig.openWeatherMapAPI.baseUrl);
+			expect(OWM_API.lang).to.equal(`lang=${appConfig.openWeatherMapAPI.lang}`);
+			expect(OWM_API.units).to.equal(`units=${appConfig.openWeatherMapAPI.units}`);
+			expect(OWM_API.appId).to.equal(`APPID=${OWM_APIKEY}`);
+		});
+	});
+
+	describe('should be an invalid request', () => {
+		it('should throw error if endpoint is not defined', (done) => {
+			OWM_API.call('')
+				.then(() => {
+					done();
+				}).catch((err) => {
+					expect(err.message).to.equal('Only absolute URLs are supported');
+					done();
+				});
+		});
+
+		it('should throw error 401 if route is invalid', (done) => {
+			OWM_API.call(`${appConfig.openWeatherMapAPI.baseUrl}/fake-path`)
+				.then(() => {
+					done();
+				}).catch((err) => {
+					const errorForInvalidRoute = '401';
+					expect(err.message).to.equal(errorForInvalidRoute);
+					done();
+				});
+		});
+	});
+
+	describe('getWeatherByIdOfCity', () => {
+		it('should throw an error if idOfCity param is not valid', (done) => {
+			try {
+				OWM_API.getWeatherByIdOfCity();
+			} catch (e) {
+				const errorMessage = 'Error: Invalid Id of city';
+				expect(e.message).to.equal(errorMessage);
+				done();
+			}
+		});
+	});
+});
