df3a9dcf187433add124d1aa94047c2c4f9fd190
Dídac García <didaquis@gmail.com>
2019-01-20 20:12:40 +0100

    Added more test for appConfiguration.js file

--- src/appConfiguration.js
+++ src/appConfiguration.js
@@ -1,11 +1,11 @@
-const appConfiguration = {
-	openweathermaps_api_configuration: {
+const appConfiguration = Object.freeze({
+	openWeatherMapsAPI: {
 		baseUrl: 'http://api.openweathermap.org/data/2.5/weather',
 		lang: 'es',
 		units: 'metric',
-		citys: [{id: 3108286, name: 'Terrassa'}, {id: 3128760, name: 'Barcelona'}]
+		cities: [{id: 3108286, name: 'Terrassa'}, {id: 3128760, name: 'Barcelona'}]
 	},
 	publishInterval: 1200000
-};
+});
 
 module.exports = appConfiguration;
--- test/test.js
+++ test/test.js
@@ -32,21 +32,55 @@ describe('Enviroment vars', () => {
 	});
 
 	it('should have valid format for Twitter API', () => {
-		const regularExpression = /[A-Za-z0-9.\-_*/|]{25,}/;
-		assert.match(twitter_consumer_key, regularExpression);
-		assert.match(twitter_consumer_secret, regularExpression);
-		assert.match(twitter_access_token_key, regularExpression);
-		assert.match(twitter_access_token_secret, regularExpression);
+		const regex_tokenOrKey = /[A-Za-z0-9.\-_*/|]{25,}/;
+		assert.match(twitter_consumer_key, regex_tokenOrKey);
+		assert.match(twitter_consumer_secret, regex_tokenOrKey);
+		assert.match(twitter_access_token_key, regex_tokenOrKey);
+		assert.match(twitter_access_token_secret, regex_tokenOrKey);
 	});
 
 	it('should have valid format for OpenWeatherMap API', () => {
-		const regularExpression = /[A-Za-z0-9.\-_*/|]{32,}/;
-		assert.match(openweathermap_apikey, regularExpression);
+		const regex_tokenOrKey = /[A-Za-z0-9.\-_*/|]{32,}/;
+		assert.match(openweathermap_apikey, regex_tokenOrKey);
 	});
 });
 
 describe('Application configuration', () => {
-	it('should be a valid object', () => {
+	it('should be an object with valid properties', () => {
 		assert.isObject(appConfig);
+
+		expect(appConfig.hasOwnProperty('openWeatherMapsAPI')).to.be.true;
+		expect(appConfig.hasOwnProperty('publishInterval')).to.be.true;
+
+		expect(appConfig.openWeatherMapsAPI.hasOwnProperty('baseUrl')).to.be.true;
+		expect(appConfig.openWeatherMapsAPI.hasOwnProperty('lang')).to.be.true;
+		expect(appConfig.openWeatherMapsAPI.hasOwnProperty('units')).to.be.true;
+		expect(appConfig.openWeatherMapsAPI.hasOwnProperty('cities')).to.be.true;
+
+		assert.isArray(appConfig.openWeatherMapsAPI.cities);
+		appConfig.openWeatherMapsAPI.cities.forEach((city) => {
+			expect(city.hasOwnProperty('id')).to.be.true;
+			expect(city.hasOwnProperty('name')).to.be.true;
+		});
+	});
+
+	it('should have a valid values', () => {
+		const regex_url = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+		assert.match(appConfig.openWeatherMapsAPI.baseUrl, regex_url);
+
+		const regex_lang = /[-a-zA-Z]{2,10}/;
+		assert.match(appConfig.openWeatherMapsAPI.lang, regex_lang);
+
+		const regex_units = /metric/;
+		assert.match(appConfig.openWeatherMapsAPI.units, regex_units);
+
+		appConfig.openWeatherMapsAPI.cities.forEach((city) => {
+			assert.isNumber(city.id);
+			expect(Number.isInteger(city.id)).to.be.true;
+			expect(city.name).to.be.a('string');
+		});
+
+		assert.isNumber(appConfig.publishInterval);
+		expect(Number.isInteger(appConfig.publishInterval)).to.be.true;
 	});
 });
