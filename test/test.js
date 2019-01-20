b8c9a737acbc7f85bd5860c9443697792bc2ccf2
Dídac García <didaquis@gmail.com>
2019-01-20 20:16:03 +0100

    Minor fix on a regular expression

--- test/test.js
+++ test/test.js
@@ -68,7 +68,7 @@ describe('Application configuration', () => {
 		const regex_url = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
 		assert.match(appConfig.openWeatherMapsAPI.baseUrl, regex_url);
 
-		const regex_lang = /[-a-zA-Z]{2,10}/;
+		const regex_lang = /[a-zA-Z]{2,10}/;
 		assert.match(appConfig.openWeatherMapsAPI.lang, regex_lang);
 
 		const regex_units = /metric/;
